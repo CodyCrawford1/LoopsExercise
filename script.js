c3be973d2377a433120fa5f3b24056a7bd03bfa8
Cody Crawford <Cody.Crawford1010@gmail.com>
2022-02-28 14:33:38 -0600

    CompletedExercise

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1
+for (i = 1; i <= 7; i++ ) {
+    console.log(i);
+}
+
+// 2
+for (i = 5; i<= 25; i +=4 ) {
+    console.log(i);
+}
+
+// 3a
+const wizards = ["Harry Potter", "Hermione Granger", "Ron Weasley"]
+
+// 3b
+for(category of wizards) {
+    console.log(wizards);
+}
+
+// 4a
+let harryPotterMovies = 0
+
+// 4b
+while (harryPotterMovies <= 8 ) {
+    harryPotterMovies++;
+    // 4c
+    console.log(harryPotterMovies);
+}
